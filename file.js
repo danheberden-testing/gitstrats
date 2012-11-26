b65c7b1e6daad3291dfe4e0ecdd1847b30309b77
Dan Heberden <danheberden@gmail.com>
2012-11-26 13:34:22 -0800

    add basic functionality to truncate at 500 chars and add ellipses

--- file.js
+++ file.js
@@ -1,5 +1,10 @@
 (function( $ ) {
   $.fn.elipse = function() {
+    var el = $( this );
+    var text = el.html();
 
+    if ( text.length > 500 ) {
+      el.html( text.slice( 0, 500 ) + '…' );
+    }
   };
 }( jQuery ));
